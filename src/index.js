50d9cd5c63e5d0d8f51a05bf29303c06cc7318e8
trandung2k1 <trandungitksnb2001@gmail.com>
2023-05-22 13:27:00 +0700

    update successfully

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ app.get('/', async (req, res) => {
     return res.status(200).json(json);
 });
 app.delete('/:id', async (req, res) => {
-    const id = req.params.id;
+    const id = +req.params.id;
     const filePath = path.join(__dirname, './db/db.json');
     const data = await fs.readFile(filePath, { encoding: 'utf8' });
     const json = JSON.parse(data);
-    const result = json.todos.filter((item) => item.id != id);
+    const result = json.todos.filter((item) => item.id !== id);
     const newData = { todos: result };
     await fs.writeFile(filePath, JSON.stringify(newData));
     return res.status(200).json(result);
